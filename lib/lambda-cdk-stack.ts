87b60bad60c739f1694899942057590e6c755ef1
Minh T Nguyen <minh.nguyen@service.vic.gov.au>
2022-09-24 20:46:42 +1000

    update gitignore

--- lib/lambda-cdk-stack.ts
+++ lib/lambda-cdk-stack.ts
@@ -1,4 +1,5 @@
 import * as cdk from 'aws-cdk-lib';
+import { LambdaRestApi } from 'aws-cdk-lib/aws-apigateway';
 import { Code, Function, Runtime } from 'aws-cdk-lib/aws-lambda';
 import { Construct } from 'constructs';
 // import * as sqs from 'aws-cdk-lib/aws-sqs';
@@ -11,6 +12,11 @@ export class LambdaCdkStack extends cdk.Stack {
       runtime: Runtime.NODEJS_14_X,
       code: Code.fromAsset('lambda'),
       handler: 'hello.handler',
+    });
+
+    // defines an API Gateway REDT API resource backed by "hello" function
+    new LambdaRestApi(this, 'ApiGateway', {
+      handler: hello,
     });
   }
 }
